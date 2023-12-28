a29aafe4dd000cc688a2415d3b061ce2d3d3bf76
Demet <demetbaspinaralkac@gmail.com>
2023-12-28 21:23:29 +0100

    v

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -20,7 +20,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
-        tags = "@US_002_Test"
+        tags = ""
 
 )
 public class Runner {
